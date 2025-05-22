b0a1e716b9b1fb47ef746eeb0662ab9134317bb4
cojestmordo <lordmada2012@gmail.com>
2025-05-22 11:10:52 +0200

    Update site branding and homepage
    - Update site title and description in consts.ts
    - Redesign homepage with new branding and tagline
    - Add dynamic blog post listing
    - Improve styling and responsiveness

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nexus Remote Hub';
+export const SITE_DESCRIPTION = 'A tech blog about remote tools, digital income, and automation tips.';
